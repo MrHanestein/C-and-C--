7f8ec597f5e610b76188ea2f7ae5e7b1b09a1b27
david Anthony <giddydanny002@gmail.com>
2024-11-07 19:04:24 +0000

    c and c++ tutorial

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    //Another edit does not seem too bad.
+   std::cout << "We would always love you akira Toriyama\n"; //He really was a good human.
+   std::cout << "You were a good father and human\n";        //He really was the best of all
+   std::cout << "Good bye." << std::endl;                    //Sail high, toriyama
+   return 0;
+}
